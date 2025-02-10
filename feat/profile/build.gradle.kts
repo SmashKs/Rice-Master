4638fdc7ebf26e5c87233bf985c6d9713c2276cf
Jieyi <pokkbaby@gmail.com>
2025-02-10 21:57:22 +0900

    feat(build): integrate androidx room and koin into project
    added androidx room and sqlite dependencies to the libs.versions.toml file. updated build.gradle.kts to incorporate room and koin plugins, libraries, and configurations. enabled ksp tasks for room compilation, added new dependencies, and set up schema directory.

--- feat/profile/build.gradle.kts
+++ feat/profile/build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.googleKsp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
@@ -36,19 +40,37 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":core:navigation"))
-            implementation(compose.material3)
-            implementation(compose.components.uiToolingPreview)
+            implementation(project(":data:user"))
+            with(compose) {
+                implementation(material3)
+                implementation(components.resources)
+                implementation(components.uiToolingPreview)
+            }
 
             implementation(libs.kotlin.stdlib)
             implementation(libs.kotlinx.serialization.json)
 
             implementation(libs.androidx.navigation.compose)
+            implementation(libs.androidx.room.runtime)
+
+            implementation(libs.sqlite.bundled)
+
+            with(libs.koin) {
+                implementation(project.dependencies.platform(bom))
+                implementation(mm.compose)
+                implementation(mm.viewmodel.navigation)
+                implementation(annotations)
+            }
+
+            implementation(libs.firebase.multiplatform.auth)
         }
 
         androidMain.dependencies {
             // Add Android-specific dependencies here. Note that this source set depends on
             // commonMain by default and will correctly pull the Android artifacts of any KMP
             // dependencies declared in commonMain.
+            implementation(project.dependencies.platform(libs.firebase.bom))
+            implementation(libs.firebase.authKtx)
         }
 
         iosMain.dependencies {
@@ -58,5 +80,36 @@ kotlin {
             // on common by default and will correctly pull the iOS artifacts of any
             // KMP dependencies declared in commonMain.
         }
+
+        named("commonMain").configure {
+            kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+        }
+
+        all {
+            languageSettings.enableLanguageFeature("ExplicitBackingFields")
+        }
+    }
+}
+
+// KSP Tasks
+dependencies {
+    add("kspCommonMainMetadata", libs.koin.ksp.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
+}
+
+ksp {
+    arg("KOIN_USE_COMPOSE_VIEWMODEL", "true")
+    arg("KOIN_CONFIG_CHECK", "true")
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    if (name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
